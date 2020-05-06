0a69a2dbe1634e0dfaa68b8d3d5946db6181b36a
LisaDrse <r0423168@student.thomasmore.be>
2020-05-06 22:21:59 +0200

    added cors to dependencies

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -14,7 +15,6 @@ mongoose.connect('mongodb+srv://Ward:Ward@cluster0-4jxji.mongodb.net/test?retryW
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
 
 const app = express();
 
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/api/v1/cryptovibes", apiTransactionsRouter);
