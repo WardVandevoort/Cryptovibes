9933ed17a38fe94ea7b20e82028afca2ed03497e
LisaDrse <r0423168@student.thomasmore.be>
2020-05-06 23:34:05 +0200

    update
    Added const jwt
    added let token and functions
    tested with postman --> SUCCESS

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const signup = async (req, res, next) =>{
-    console.log(req.body);
+    //console.log(req.body);
 
     let username = req.body.username; //uit postman momenteel
     let password = req.body.password;
@@ -9,8 +10,18 @@ const signup = async (req, res, next) =>{
     const user = new User({username: username});
     await user.setPassword(password);
     await user.save().then(result => {
+        console.log(result._id);
+        //token toekennen
+        let token = jwt.sign({
+            uid:result._id,
+            username: username
+        }, "MyVerySecretWord"); //hardcoded-> nog te vervangen 
+
         res.json({
-            "status": "success"
+            "status": "success",
+            "data": {
+                 "token": token
+            }
         })
     }).catch(error =>{
         res.json({
