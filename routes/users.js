0a37c28777948b8a0a1421e172dd982e4110f549
LisaDrse <r0423168@student.thomasmore.be>
2020-05-06 17:28:04 +0200

    started on login and signup backend
    added file auth.js to folder controllers
    added router.post/signup to users.js for usage of passport  and const authController to users.js
    added passport dependencies to packages

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const authController = require('../controllers/auth');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res, next) =>{
+  //signup
+  //username uit request
+  //ww uit request
+  //bcrypt encrypt
+  //databank
+
 });
 
 module.exports = router;
