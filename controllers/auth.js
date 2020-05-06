8894a38ccdb049d8c9af0e3ecab37821b4aa39de
LisaDrse <r0423168@student.thomasmore.be>
2020-05-06 19:33:53 +0200

    added following
    - async function for username and password
    - console log (req.body) --> for testing in postman

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,22 @@
 const User = require('../models/User');
-const passport = require('../passport/passport');
 
-const signup =(req, res, next) =>{
+const signup = async (req, res, next) =>{
+console.log(req.body);
 
+    let username = req.body.username; //uit postman momenteel
+    let password = req.body.password;
+
+    const user = new User({username: username});
+    await user.setPassword(password);
+    await user.save().then(result => {
+        res.json({
+            "status": "success"
+        })
+    }).catch(error =>{
+        res.json({
+            "status": "error"
+        })
+    });
 };
 
 module.exports.signup = signup;
