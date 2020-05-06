4f80d2efe8e0f76487b0e7a9da38c9050007894b
LisaDrse <r0423168@student.thomasmore.be>
2020-05-06 23:35:09 +0200

    Update passport.js
    added WEBTOKEN strategy for authentication --> tested with postman --> correct token: SUCCES and false token: UNAUTHORIZED

--- passport/passport.js
+++ passport/passport.js
@@ -7,3 +7,30 @@ passport.use(User.createStrategy());
 //inlezen en uitlezen users (niet van toepassing)
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+//WEBTOKEN strategie (JWT )
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'MyVerySecretWord';
+//opts.issuer = 'accounts.examplesoft.com';
+//opts.audience = 'yoursite.net'; 
+
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({
+        _id: jwt_payload.uid
+    }, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+})); 
+
+module.exports = passport;
