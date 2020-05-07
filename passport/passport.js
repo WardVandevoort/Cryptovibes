811a14a4ce00f156e55efff6583b05526f4b3643
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-05-07 17:08:53 +0200

    Added redirect to login when token in incorrect

--- passport/passport.js
+++ passport/passport.js
@@ -23,7 +23,7 @@ passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
         _id: jwt_payload.uid
     }, function(err, user) {
         if (err) {
-            console.log("foute token!!!");
+            res.writeHead({ "Location": "https://cryptovibes.herokuapp.com/users/login" });
             return done(err, false);
         }
         if (user) {
