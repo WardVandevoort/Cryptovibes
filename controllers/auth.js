66338a7e2e7f42756bd787928844753a0c83444f
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-05-07 15:03:34 +0200

    Removed result from login

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,16 +54,16 @@ const login = async (req, res, next) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    const user = await User.authenticate()(email, password).then(result => {
-        if(!result.user){
+    const user = await User.authenticate()(email, password).then(user => {
+        if(!user){
             return res.json({
                 "status" : "failed",
                 "message": "Login failed"
             })
         }
         let token = jwt.sign({
-            uid:result.user._id,
-            username:result.user.username
+            uid:user._id,
+            username:user.username
         }, config.get('jwt.secret'));
          
         return res.json({
