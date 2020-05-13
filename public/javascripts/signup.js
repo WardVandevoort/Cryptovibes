0f602450d92d791b4f2cb97918fba5b73a33aede
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-05-13 17:28:47 +0200

    Added email verification

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,34 +1,42 @@
 const base_url = "https://cryptovibes.herokuapp.com/";
 
 var btnSignup = document.querySelector("#submit").addEventListener("click", (e) => {
-    let username = document.querySelector('#email').value;
+    let username;
     let firstname = document.querySelector('#firstname').value;
     let lastname = document.querySelector('#lastname').value;
     let email = document.querySelector('#email').value;
     let password = document.querySelector('#password').value;
+    let validEmail = email.indexOf("@student.thomasmore.be");
 
+    if(validEmail != -1){
+        username = email.slice(0, validEmail-1);
 
-    fetch(base_url + 'users/signup', {
-        method: "post",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "username": username,
-            "firstname": firstname,
-            "lastname": lastname,
-            "email": email,
-            "password": password,
-            "wallet": 100
+        fetch(base_url + 'users/signup', {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "username": username,
+                "firstname": firstname,
+                "lastname": lastname,
+                "email": email,
+                "password": password,
+                "wallet": 100
+            })
+            
+        }).then(response => {
+            return response.json();
+        }).then(json => {
+            if (json.status ==="success"){
+                let token = json.data.token;
+                localStorage.setItem("token", token);
+                window.location.href = base_url;
+            }
         })
-        
-    }).then(response => {
-        return response.json();
-    }).then(json => {
-        if (json.status ==="success"){
-            let token = json.data.token;
-            localStorage.setItem("token", token);
-            window.location.href = base_url;
-        }
-    })
+
+    }
+
+
+    
 });
