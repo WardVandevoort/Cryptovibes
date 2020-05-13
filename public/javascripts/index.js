111918e1d4209bd4f8c70ecfab1bf1abfae2ff64
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-05-13 17:02:24 +0200

    Deleted error message

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,6 +8,6 @@ fetch("https://cryptovibes.herokuapp.com/api/v1/cryptovibes/", {
 }).then(json => {
      console.log(json);
 }).catch(err => {
-     console.log(err);
+     
      window.location.href = "https://cryptovibes.herokuapp.com/login";
 })
