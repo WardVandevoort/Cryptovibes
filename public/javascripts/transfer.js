26a3470b99b5df7661fcef2ec9cf591a85df4179
LisaDrse <r0423168@student.thomasmore.be>
2020-05-14 16:24:54 +0200

    added websockets
    added websockets to transfer.js

--- public/javascripts/transfer.js
+++ public/javascripts/transfer.js
@@ -1,1 +1,14 @@
 const base_url = "https://cryptovibes.herokuapp.com/";
+
+let primus = Primus.connect(base_url, {
+    reconnect: {
+        max: Infinity, 
+        min: 500, 
+        retries: 10 
+    }
+});
+
+document.querySelector("#submit").addEventListener("click", function (e) {
+    
+  });
+
