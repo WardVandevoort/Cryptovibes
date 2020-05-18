6a45670c7030261ccb06988da151bc8eda9c5f0c
LisaDrse <r0423168@student.thomasmore.be>
2020-05-18 23:29:59 +0200

    css and code
    changed margin on logo in transfer.css
    
    deleted comment around getusers

--- routes/api/v1/cryptovibes.js
+++ routes/api/v1/cryptovibes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//const userController = require('../../../controllers/api/v1/cryptovibes')
+const userController = require('../../../controllers/api/v1/cryptovibes')
 const transactionsController = require("../../../controllers/api/v1/cryptovibes");
 
 router.get('/:id', transactionsController.getTransaction);  //krijg één transactie
@@ -9,6 +9,6 @@ router.post('/', transactionsController.createTransaction);  //new transaction
 
 router.get('/', transactionsController.getTransactions);  //alle tranacties
 
-//router.get('/', userController.getUsers);  //alle users
+router.get('/', userController.getUsers);  //alle users
 
 module.exports = router;
