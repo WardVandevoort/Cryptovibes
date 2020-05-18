ea5902bc928997fec9ab2a8c4eeb109146fb0e8c
LisaDrse <r0423168@student.thomasmore.be>
2020-05-18 22:27:04 +0200

    Update transfer.js
    removed hardcoded data

--- controllers/api/v1/transfer.js
+++ controllers/api/v1/transfer.js
@@ -48,7 +48,7 @@ const createTransaction = (req, res, next) => {
 
 //get alle transactions
 const getTransactions = (req, res) => {
-    Transaction.find({$or:[{"receiver_id": "r0746456"}, {"sender_id": "r0746456"}]}, (err, docs) => {
+    Transaction.find({$or:[{"receiver_id": " "}, {"sender_id": " "}]}, (err, docs) => {
         //console.log("hello??");
         if(!err){
             res.json({
