64df1bf506bc9df000cc327f13ee25480793707c
LisaDrse <r0423168@student.thomasmore.be>
2020-05-18 21:35:31 +0200

    Update transfer.js

--- public/javascripts/transfer.js
+++ public/javascripts/transfer.js
@@ -16,7 +16,7 @@ document.querySelector(".btn").addEventListener("click", function (e) {
     let quantity = document.querySelector('.amount').value;
     let message = document.querySelector('.message').value;
     
-    fetch(base_url + 'routes/transfer', {
+    fetch(base_url + 'transfer/', + quantity {
         method: "post",
         headers:{'Content-Type':'application/json'},
         body: JSON.stringify({
