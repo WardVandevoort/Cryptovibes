03c8d414fe6b133d86d3ff2fd0b7f9c16c488648
LisaDrse <r0423168@student.thomasmore.be>
2020-05-18 13:16:11 +0200

    Update transfer.js
    added transfer function to file

---
+++ public/javascripts/transfer.js
@@ -0,0 +1,27 @@
+const base_url ="https://cryptovibes.herokuapp.com/";
+
+var btnTransfer = document.querySelector(".btn").addEventListener("click", (e)=>{
+    let receiver_id = document.querySelector('.receiver').value;
+    //let sender_id = document.querySelector('')
+    let type_id = document.querySelector('.reason').value;
+    let quantity = document.querySelector('.amount').value;
+    let message = document.querySelector('.message').value;
+    
+    fetch(base_url + 'transfer/transfer', {
+        method: "post",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            "receiver_id": receiver_id,
+            "type_id": type_id,
+            "quantity": quantity,
+            "message": message
+        })
+    }). then(response =>{
+        return response.json();
+    }).then(json => {
+        console.log(json);
+
+    })
+})
