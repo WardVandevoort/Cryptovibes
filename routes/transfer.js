3e881d52b17639c585498e6935c1af27e2a38a24
LisaDrse <r0423168@student.thomasmore.be>
2020-05-18 13:17:19 +0200

    Added routes to transfer file
    added const transferController for route

--- routes/transfer.js
+++ routes/transfer.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const transferController = require('../controllers/api/v1/cryptovibes');
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/transfer', function(req, res, next) {
   res.render('transfer');
 });
 
