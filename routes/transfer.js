dc03e0d40974e119baa24f9139589d2132651a92
LisaDrse <r0423168@student.thomasmore.be>
2020-05-18 23:07:10 +0200

    replaced transferfile of routes

--- routes/transfer.js
+++ routes/transfer.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const transactionsController = require("../../../controllers/transfer");
+const transactionsController = require("../controllers/transfer");
 
 router.get('/:id', transactionsController.getTransaction);  //krijg één transactie
 
