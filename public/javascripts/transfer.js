039d9ea599834732edc44f76b5cba5e2121b0dcb
LisaDrse <r0423168@student.thomasmore.be>
2020-05-18 21:37:21 +0200

    Update transfer.js
    changed code for transfer route

--- public/javascripts/transfer.js
+++ public/javascripts/transfer.js
@@ -16,7 +16,7 @@ document.querySelector(".btn").addEventListener("click", function (e) {
     let quantity = document.querySelector('.amount').value;
     let message = document.querySelector('.message').value;
     
-    fetch(base_url + 'transfer/', + quantity {
+    fetch(base_url + 'transfer/',{
         method: "post",
         headers:{'Content-Type':'application/json'},
         body: JSON.stringify({
