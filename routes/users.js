a745329a2faed7d86061eca0009ca427ef637663
LisaDrse <r0423168@student.thomasmore.be>
2020-05-19 15:55:55 +0200

    Update users.js
    update route user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const authController = require('../controllers/auth');
+const userController = require('../../../controllers/api/v1/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -17,5 +18,7 @@ router.get('/signup', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
+
+router.get('/', userController.getUsers);  //all users
 
 module.exports = router;
