2fdbda5887df834404cb13bffe247da04e0e9980
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-05-19 15:05:00 +0200

    Changed some code in index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,17 +9,18 @@ fetch("https://cryptovibes.herokuapp.com/api/v1/cryptovibes/"
  }).then(json => {
      json.data.transactions.forEach(transaction => {
           let decoded = JSON.parse(atob(token.split('.')[1]));
-          let locate = decoded.indexOf("@student.thomasmore.be");
-          let tokenData = decoded.slice(0, locate);
+          let username = decoded.username;
+          let locate = username.indexOf("@student.thomasmore.be");
+          let tokenData = username.slice(0, locate);
 
           console.log(decoded);
           console.log(tokenData);
-         if (transaction.receiver_id == token.username) {
+         if (transaction.receiver_id == tokenData) {
              payment = `<div class="deposit ${transaction.receiver_id} ${token.username}">
              <p>Cryptocoin +${transaction.quantity}</p>
              </div>`;
           } 
-          else if(transaction.sender_id == token.username){
+          else if(transaction.sender_id == tokenData){
                payment = `<div class="withdrawal">
                <p>Cryptocoin -${transaction.quantity}</p>
                </div>`;
