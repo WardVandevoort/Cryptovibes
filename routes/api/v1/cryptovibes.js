159cbe48ea3f0a71570e911ecaec63180638ed6b
LisaDrse <r0423168@student.thomasmore.be>
2020-05-19 17:11:01 +0200

    changed router name to route

--- routes/api/v1/cryptovibes.js
+++ routes/api/v1/cryptovibes.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const route = express.Router();
+
 const userController = require('../../../controllers/api/v1/cryptovibes');
 const transactionsController = require("../../../controllers/api/v1/transactions");
 
 router.get('/getUsers', userController.getUsers);  //all users
 
-router.get('/:id', transactionsController.getOneTransaction);  //get one transaction
+route.get('/:id', transactionsController.getOneTransaction);  //get one transaction
 
-router.post('/', transactionsController.createTransaction);  //new transaction
+route.post('/', transactionsController.createTransaction);  //new transaction
 
-router.get('/', transactionsController.getTransactions);  //all tranactions
+route.get('/', transactionsController.getTransactions);  //all tranactions
 
 module.exports = router;
+module.exports = route;
 
