7ff9ade77cc2fb5605b7f0cca61d42cb90e2dc47
LisaDrse <r0423168@student.thomasmore.be>
2020-05-19 02:33:50 +0200

    changes made
    added folder data with data.json in
    finished function of inputfield in transfer.js

--- public/javascripts/transfer.js
+++ public/javascripts/transfer.js
@@ -5,7 +5,7 @@ const matchUser = document.querySelector('.matchUser');
 
 //search receiver and filter it
 const searchReceivers = async searchText => {
-    const res = await fetch ('../../data.json');
+    const res = await fetch ('../../data/data.json');
     const names = await res.json();
 
     //console.log(names);
@@ -13,10 +13,26 @@ const searchReceivers = async searchText => {
     //get matches 
     let matches = names.filter(name => {
         const regex = new RegExp(`^${searchText}`, 'gi');
-        return name.email.match(regex) || name.abbr.match(regex);
+        return name.name.match(regex) || name.abbr.match(regex);
     });
 
-    console.log(names);
-}
+    if(searchText.length === 0){
+        matches = [];
+        matchUser.innerHTML='';
+    }
+    outputHtml(matches);
+};
+const outputHtml = matches => {
+    if(matches.length > 0){
+        const html = matches.map(match =>`
+        <div class="card">
+            <h4>${match.name} (${match.abbr}) <span class="summary">${match.capital}</span></h4>
+        </div>`).join('');
 
-receivername.addEventListener('input',searchReceivers(receivername.value));
+        matchUser.innerHTML= html; 
+    }
+};
+
+
+
+receivername.addEventListener('input',() => searchReceivers(receivername.value));
