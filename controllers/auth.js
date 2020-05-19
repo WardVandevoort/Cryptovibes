7731d8a2a22bfb7cd5d29829d2ffa4f0eb8ae3b7
LisaDrse <r0423168@student.thomasmore.be>
2020-05-19 20:37:48 +0200

    testing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+//const User = require('../models/User');
 const Users = require('../models/Users');
 const jwt = require('jsonwebtoken');
 const config = require('config');
