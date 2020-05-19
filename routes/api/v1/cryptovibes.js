4a289f94075111f8c2d76c0813ac3765145c0797
LisaDrse <r0423168@student.thomasmore.be>
2020-05-19 18:16:22 +0200

    Update cryptovibes.js

--- routes/api/v1/cryptovibes.js
+++ routes/api/v1/cryptovibes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const route = express.Router();
 
-//const userController = require('../../../controllers/api/v1/cryptovibes');
+const userController = require('../../../controllers/api/v1/cryptovibes');
 const transactionsController = require("../../../controllers/api/v1/cryptovibes");
 
-router.get('/', transactionsController.getUsers);  //all users
+router.get('/users', userController.getUsers);  //all users
 
 route.get('/:id', transactionsController.getOneTransaction);  //get one transaction
 
