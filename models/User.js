be725a04025468ff2d7f55256bd09ad24961cc0b
LisaDrse <r0423168@student.thomasmore.be>
2020-05-19 23:43:23 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -12,7 +12,4 @@ const User = new Schema({
 });
 User.plugin(passportLocalMongoose);
 
-const Users = mongoose.model("users", User);
-module.exports = Users;
-
 module.exports = mongoose.model('users', User);
