432af29f1e79b52856ae25b0d396eff0290a3701
LisaDrse <r0423168@student.thomasmore.be>
2020-05-21 15:58:25 +0200

    test

--- controllers/api/v1/cryptovibes.js
+++ controllers/api/v1/cryptovibes.js
@@ -24,12 +24,12 @@ const getOneTransaction = (req, res) => {
 const createTransaction = (req, res, next) => {
     let transaction = new Transaction();
 
-    transaction.quantity = req.body.quantity;
-    transaction.receiver_id = req.body.receiverName;
-    transaction.sender_id = req.body.sender_id;
+    transaction.quantity = req.user.quantity;
+    transaction.receiver_id = req.user.receiver_id;
+    transaction.sender_id = req.user.sender_id;
     transaction.date = new Date().toLocaleDateString()
-    transaction.type_id = req.body.type_id;
-    transaction.message = req.body.message;
+    transaction.type_id = req.user.type_id;
+    transaction.message = req.user.message;
     transaction.save((err, doc) => {
         if(err){
             //console.log("test");
