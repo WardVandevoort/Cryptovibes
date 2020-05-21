360dc6cb02da32d8c7a210bb95ece1ab0f863370
LisaDrse <r0423168@student.thomasmore.be>
2020-05-21 10:36:02 +0200

    Update transfer.js

--- public/javascripts/transfer.js
+++ public/javascripts/transfer.js
@@ -1,7 +1,9 @@
+const base_url ="https://cryptovibes.herokuapp.com/";
+
 document.querySelector(".submit").addEventListener('click', (e) =>{
     let receiver = document.querySelector(".searchReceiver").value;
-    let reason = document.querySelector(".reason").value;
-    let amount = document.querySelector(".amount").value;
+    let type_id = document.querySelector(".reason").value;
+    let quantity = document.querySelector(".amount").value;
     let message = document.querySelector(".message").value;
 
     fetch(base_url + 'transfers'),{
@@ -11,8 +13,8 @@ document.querySelector(".submit").addEventListener('click', (e) =>{
         },
         body: JSON.stringify({
             "receiver": receiver,
-            "reason": reason,
-            "amount":amount,
+            "type_id": type_id,
+            "quantity":quantity,
             "message": message
         })
     }.then(response => {
