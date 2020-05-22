113bc41ae469492d77d40e0dda66b593b621ca99
LisaDrse <r0423168@student.thomasmore.be>
2020-05-22 18:42:47 +0200

    Gulp by Lisa
    added 3 functions to gulpfile: sass2css, minify-css and imagemin
    added gulp to package
    added gulp to package.json (npm start dev)

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+sass.compiler = require('node-sass');
+
+sass2css = function(){
+    return src('./public/stylesheets/sass/app.css')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./dist'));
+};
+
+gulp.task('minify-css', () => {
+return gulp.src('./public/stylesheets/sass/app.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('imagemin', ()=>{
+    return gulp.src('./public/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist'));
+})
+
+exports.default = function() {
+    // You can use a single task
+    watch('./public/stylesheets/sass/**/*.scss', sass2css);
+    watch('./public/stylesheets/sass/**/*.scss', task);
+  };
