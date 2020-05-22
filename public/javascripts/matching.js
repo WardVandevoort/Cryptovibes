5f14276b6db17bc2da611cf061ed4230e066e169
LisaDrse <r0423168@student.thomasmore.be>
2020-05-22 17:37:04 +0200

    Update matching.js
    update autofill

--- public/javascripts/matching.js
+++ public/javascripts/matching.js
@@ -1,36 +1,33 @@
-const base_url ="https://cryptovibes.herokuapp.com/";
-
-const receivername = document.querySelector('.receiverName');
+const receiverName = document.querySelector('.receiverName');
 const matchUser = document.querySelector('.matchUser');
 
 //search receiver and filter it
-const searchReceivers = async searchText => {
-    const res = await fetch (base_url + 'cryptovibes/users');
+    const searchReceivers = async searchText => {
+
+    const res = await fetch (base_url + 'api/v1/cryptovibes/users');
     const receivers = await res.json();
 
     //console.log(receivers);
-
+    
     //get matches 
     let matches = receivers.filter(receiver => {
         const regex = new RegExp(`^${searchText}`, 'gi');
-        return receiver.email.match(regex);
+        return receiver.receiverName.match(regex);
+        console.log(matches);
     });
-
-    //console.log(matches);
 
     if(searchText.length === 0){
         matches = [];
         matchUser.innerHTML='';
     }
     outputHtml(matches);
-}
 
-const outputHtml = matches => {
+    const outputHtml = matches => {
     if(matches.length > 0){
-        const html = matches.map(match =>`<a class ='${match.email}' href = " ">${match.email}</a>`).join('');
+        const html = matches.map(match =>`<a class ='${match.receiverName}' href = " ">${match.receiverName}</a>`).join('');
         matchUser.innerHTML= html; 
+        }
     }
-};
-
-receivername.addEventListener('input',() => searchReceivers(receivername.value));
-
+    
+receiverName.addEventListener('input',() => searchReceivers(receiverName.value));
+    }
