4c5c60e09fe925a3e7f864ddc30da775fe73a4e9
Ward Vandevoort <vandevoort.ward@gmail.com>
2020-05-22 17:33:52 +0200

    Trying to fix bug in transfer.js

--- public/javascripts/transfer.js
+++ public/javascripts/transfer.js
@@ -1,4 +1,13 @@
 const base_url ="https://cryptovibes.herokuapp.com/";
+let token = localStorage.getItem("token");
+
+if(token == null){
+   
+    window.location.href = "https://cryptovibes.herokuapp.com/users/login";
+}
+
+let decoded = JSON.parse(atob(token.split('.')[1]));
+let tokenData = decoded.username;
 
 document.querySelector(".btn").addEventListener('click', (e) =>{
     let receiver_id = document.querySelector(".receiverName").value;
@@ -15,6 +24,7 @@ document.querySelector(".btn").addEventListener('click', (e) =>{
         },
         body: JSON.stringify({
             "receiver_id": receiver_id,
+            "sender_id": tokenData,
             "type_id": type_id,
             "quantity": quantity,
             "message": message
