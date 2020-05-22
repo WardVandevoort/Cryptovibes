bfe70038559a62ed0c8a90b1f29c53870103b460
LisaDrse <r0423168@student.thomasmore.be>
2020-05-22 20:27:45 +0200

    fixiing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,22 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
+//const imagemin = require('gulp-imagemin');
 sass.compiler = require('node-sass');
 
 sass2css = function(){
     return src('./public/stylesheets/sass/app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('/dist'));
+    .pipe(sass({ outputStyle: "compressed" }).on('error', sass.logError))
+    .pipe(dest('./public/dist/css/'));
 };
 
-gulp.task('minify-css', () => {
-return gulp.src('./public/stylesheets/sass/app.scss')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('/dist/app.css'));
-});
-
-gulp.task('imagemin', ()=>{
+/* imagemin = function (){
     return gulp.src('./public/svg')
     .pipe(imagemin())
     .pipe(gulp.dest('/dist/app.css'));
-})
+} */
 
 exports.default = function() {
     // You can use a single task
     watch('./public/stylesheets/sass/**/*.scss', sass2css);
-    watch('./public/stylesheets/sass/**/*.scss', task);
-    watch('./public/stylesheets/sass/**/*.scss', task);
-  };
+    //watch('./public/stylesheets/sass/**/*.scss', imagemin);
+}
